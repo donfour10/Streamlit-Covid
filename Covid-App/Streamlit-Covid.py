1c4e31a759c40d0d5c4fa7629eafe8a1b1870df2
donfour10 <dustin@werner-sippe.de>
2020-03-21 13:32:05 +0100

    added new selectbox to choose stats

--- Covid-App/Streamlit-Covid.py
+++ Covid-App/Streamlit-Covid.py
@@ -8,16 +8,17 @@ def main():
     sidebar()
 
 def sidebar():
-    selectDataSource = st.sidebar.selectbox('Choose your preferred DataSource',('RKI','WHO'))
+    selectDataSource = st.sidebar.selectbox('Choose your preferred DataSource',('RKI','John Hopkins'))
     if selectDataSource == 'RKI':
         # load csv with RKI data
         pass
-    if selectDataSource == 'WHO':
-        # load csv with WHO Data
-        df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv', error_bad_lines=False)
-        st.markdown('Confirmed Cases')
-        st.dataframe(df) # just printing df - later i want to produce a chart
-        pass
+    if selectDataSource == 'John Hopkins':
+        # load csv with CCSE Data
+        selectStats = st.sidebar.selectbox('Choose the statistics you want to look at',('Confirmed cases', 'Deaths', 'Recovered'))
+        if selectStats == 'Confirmed cases':
+            df_confirmed_cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv', error_bad_lines=False)
+            st.markdown('Confirmed Cases')
+            st.dataframe(df_confirmed_cases)  # just printing df - later i want to produce a chart
 
 def preparingDF(): # maybe no need for this method
     # I want to prepare the df in this method (delete rows,columns etc.)
