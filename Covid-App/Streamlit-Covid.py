8385445a7a488815ea83669c8e4cb82282b2905c
donfour10 <55831555+donfour10@users.noreply.github.com>
2020-03-21 01:15:59 +0100

    Add files via upload

---
+++ Covid-App/Streamlit-Covid.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+import csv
+
+def main():
+    st.title("Visualization of current Statistics regarding COVID-19")
+    st.markdown('Developed by Dustin Werner & Fabian-Malte Moeller #WeVSVirus')
+    sidebar()
+
+def sidebar():
+    selectDataSource = st.sidebar.selectbox('Choose your preferred DataSource',('RKI','WHO'))
+    if selectDataSource == 'RKI':
+        # load csv with RKI data
+        pass
+    if selectDataSource == 'WHO':
+        # load csv with WHO Data
+        df = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv', error_bad_lines=False)
+        st.markdown('Confirmed Cases')
+        st.dataframe(df) # just printing df - later i want to produce a chart
+        pass
+
+def preparingDF(): # maybe no need for this method
+    # I want to prepare the df in this method (delete rows,columns etc.)
+    # Also the markdown of the plots we can implement in here! -> can discuss this point
+    pass
+
+main()
