a905dbe1dc50e65a5653c54bc8de990d9e60e537
donfour10 <dustin@werner-sippe.de>
2020-03-23 18:06:26 +0100

    if line of csv data has error, automatic delete

--- Covid-App/Streamlit-Covid.py
+++ Covid-App/Streamlit-Covid.py
@@ -16,7 +16,7 @@ def sidebar():
         # load csv with CCSE Data
         df_confirmed_cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv', error_bad_lines=False)
         df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv', error_bad_lines=False)
-        df_recovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
+        df_recovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv', error_bad_lines=False)
         selectVisualization = st.sidebar.selectbox('Which visualization do you want to look at?', ('-','line chart','world map'))
         if selectVisualization == 'line chart':
             countryList = df_confirmed_cases['Country/Region'].tolist()
