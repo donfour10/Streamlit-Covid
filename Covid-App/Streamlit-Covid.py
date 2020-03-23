0c286d381bbae1c3e92066189cdf6b7f89cf7b2c
donfour10 <dustin@werner-sippe.de>
2020-03-23 17:46:49 +0100

    change structure to facilitate world map coding

--- Covid-App/Streamlit-Covid.py
+++ Covid-App/Streamlit-Covid.py
@@ -17,55 +17,59 @@ def sidebar():
         df_confirmed_cases = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv', error_bad_lines=False)
         df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv', error_bad_lines=False)
         df_recovered = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
-        countryList = df_confirmed_cases['Country/Region'].tolist()
-        countryList = list(set(countryList))
-        selectCountry = st.sidebar.selectbox('Please choose your Country/Region',countryList)
-        provinceList = df_confirmed_cases.loc[df_confirmed_cases['Country/Region']== selectCountry]['Province/State'].tolist()
-        if provinceList != [np.nan]:
-            province_available = True
-            selectProvince = st.sidebar.selectbox('Please choose your Province/State', provinceList)
-        else:
-            province_available = False
-        datestrList = list(df_confirmed_cases.loc[:,'1/22/20':])
-        x = []
-        for date in datestrList:
-            datetime_obj = datetime.strptime(date, '%m/%d/%y')
-            # datetime_obj = np.datetime64(datetime_obj)
-            x.append(datetime_obj)
-        p = figure(
-                title = 'line chart',
-                x_axis_label = 'Date',
-                x_axis_type = 'datetime',
-                # x_range = x,
-                y_axis_label = 'Number of persons'
-            )
-        ckb_cc = st.sidebar.checkbox('cofirmed cases', value = True)
-        if ckb_cc:
-            # st.write(df_confirmed_cases)  # just printing df - later i want to produce a chart
-            if province_available == False:
-                y_cc = list(df_confirmed_cases.loc[df_confirmed_cases['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
+        selectVisualization = st.sidebar.selectbox('Which visualization do you want to look at?', ('-','line chart','world map'))
+        if selectVisualization == 'line chart':
+            countryList = df_confirmed_cases['Country/Region'].tolist()
+            countryList = list(set(countryList))
+            selectCountry = st.sidebar.selectbox('Please choose your Country/Region',countryList)
+            provinceList = df_confirmed_cases.loc[df_confirmed_cases['Country/Region']== selectCountry]['Province/State'].tolist()
+            if provinceList != [np.nan]:
+                province_available = True
+                selectProvince = st.sidebar.selectbox('Please choose your Province/State', provinceList)
             else:
-                y_cc = list(df_confirmed_cases.loc[(df_confirmed_cases['Country/Region']==selectCountry) & (df_confirmed_cases['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
-            p.line(x,y_cc, legend = 'confirmed cases' ,line_width = 2)
+                province_available = False
+            datestrList = list(df_confirmed_cases.loc[:,'1/22/20':])
+            x = []
+            for date in datestrList:
+                datetime_obj = datetime.strptime(date, '%m/%d/%y')
+                # datetime_obj = np.datetime64(datetime_obj)
+                x.append(datetime_obj)
+            p = figure(
+                    title = 'line chart',
+                    x_axis_label = 'Date',
+                    x_axis_type = 'datetime',
+                    # x_range = x,
+                    y_axis_label = 'Number of persons'
+                )
+            ckb_cc = st.sidebar.checkbox('cofirmed cases', value = True)
+            if ckb_cc:
+                # st.write(df_confirmed_cases)  # just printing df - later i want to produce a chart
+                if province_available == False:
+                    y_cc = list(df_confirmed_cases.loc[df_confirmed_cases['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
+                else:
+                    y_cc = list(df_confirmed_cases.loc[(df_confirmed_cases['Country/Region']==selectCountry) & (df_confirmed_cases['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
+                p.line(x,y_cc, legend = 'confirmed cases' ,line_width = 2)
 
-        ckb_d = st.sidebar.checkbox('deaths', value= True)
-        if ckb_d:
-            # st.write(df_deaths)
-            if province_available == False:
-                y_d = list(df_deaths.loc[df_deaths['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
-            else:
-                y_d = list(df_deaths.loc[(df_deaths['Country/Region']==selectCountry) & (df_deaths['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
-            p.line(x, y_d, legend='deaths', line_width=2, color= 'red')
+            ckb_d = st.sidebar.checkbox('deaths', value= True)
+            if ckb_d:
+                # st.write(df_deaths)
+                if province_available == False:
+                    y_d = list(df_deaths.loc[df_deaths['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
+                else:
+                    y_d = list(df_deaths.loc[(df_deaths['Country/Region']==selectCountry) & (df_deaths['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
+                p.line(x, y_d, legend='deaths', line_width=2, color= 'red')
 
-        ckb_r = st.sidebar.checkbox('recovered', value = True)
-        if ckb_r:
-            if province_available == False:
-                y_r = list(df_recovered.loc[df_recovered['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
-            else:
-                y_r = list(df_recovered.loc[(df_recovered['Country/Region']==selectCountry) & (df_recovered['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
-            p.line(x, y_r, legend='recovered', line_width=2, color = 'green')
-            # st.write(df_recovered)
-        st.bokeh_chart(p)
+            ckb_r = st.sidebar.checkbox('recovered', value = True)
+            if ckb_r:
+                if province_available == False:
+                    y_r = list(df_recovered.loc[df_recovered['Country/Region']==selectCountry].loc[:,'1/22/20':].iloc[0])
+                else:
+                    y_r = list(df_recovered.loc[(df_recovered['Country/Region']==selectCountry) & (df_recovered['Province/State']==selectProvince)].loc[:,'1/22/20':].iloc[0])
+                p.line(x, y_r, legend='recovered', line_width=2, color = 'green')
+                # st.write(df_recovered)
+            st.bokeh_chart(p)
+        if selectVisualization == 'world map':
+            # implemet world map visualization with circles to hover over
 
 def preparingDF(): # maybe no need for this method
     # I want to prepare the df in this method (delete rows,columns etc.)
