b30b99ffa8673ac49c603f7858203993a0649086
donfour10 <dustin@werner-sippe.de>
2020-03-29 17:21:39 +0200

    comment for province list bug

--- Covid-App/Streamlit-Covid.py
+++ Covid-App/Streamlit-Covid.py
@@ -33,6 +33,9 @@ def sidebar():
             selectCountry = st.sidebar.selectbox('Please choose your Country/Region',countryList)
             provinceList = df_confirmed_cases.loc[df_confirmed_cases['Country/Region']== selectCountry]['Province/State'].tolist()
             provinceList.sort()
+            # for province in provinceList:
+            #     if province == np.nan:
+            #         province = selectCountry
             if provinceList != [np.nan]:
                 province_available = True
                 selectProvince = st.sidebar.selectbox('Please choose your Province/State', provinceList)
